946a2c6c785466df71d51d0fd7960919c86f00a0
saragp18 <grijalbapsaras.cla@gmail.com>
2025-02-21 12:57:43 -0500

    entrega

--- src/ui/Atlas.java
+++ src/ui/Atlas.java
@@ -1,8 +1,8 @@
 /*
 * Analisis
 * Descripcion del programa:
-* Entradas:
-* Salidas: 
+* Entradas:nombre, edad, documento de identidad, ciudad de origen, medio de transporte, noches que se queda;
+* Salidas: total de la compra 
 * Ejemplo:
 */
 
@@ -10,13 +10,13 @@ package ui;
 
 import java.util.Scanner;
 
-public class Atlas {
-
+public class Atlas { 
 	private Scanner escaner;
 
     // Declaracion de constantes (completar)
     final double COSTO_TRAYECTO_AVION = 250000;
-
+    final double COSTO_POR_NOCHE = 150000;
+    final double COSTO_POR_SERVICIO = 0.1;
 	private Atlas() {
 		escaner = new Scanner(System.in);
 	}
@@ -24,20 +24,27 @@ public class Atlas {
 	
 	public void run()
 	{   
-        // Declaracion de info a pedir del usuario (completar)
-        String nombre;
+        int edad, medioDeTransporte, NochesQueSeQueda;
+        String nombre, ciudadOrigen;
         // Saludo
         System.out.println("Bienvenido al programa de viajes EVENTOS ATLAS realizado por los estudiantes de Algoritmos y programacion I de la Universidad ICESI.");
-        // Pedir info al usuario (completar)
+        System.out.println("Por favor complete la siguiente informacion: ");
         System.out.println("Cual es tu nombre?");
         nombre = escaner.nextLine();
-
-        // ...
         System.out.println("Bienvenido " + nombre + "!");
-        // ...
-
+        System.out.println("Cual es tu ciudad de origen?");
+        ciudadOrigen = escaner.nextLine();
+        System.out.println("muy bien tu ciudad de origen es " + ciudadOrigen);
+        System.out.println("Cual es tu edad?" + nombre);
+        edad = escaner.nextInt();
+        System.out.println("Cual es tu medio de transporte?" + nombre);
+        medioDeTransporte = escaner.nextInt();
+        System.out.println("Cuantas noches te quedaras?" + nombre);
+        NochesQueSeQueda= escaner.nextInt();
+        
+        
         // Declaracion de salidas calculadas (completar)
-        double totalTransporte;
+        double totalTransporte, totalHospedaje;
         // Calculo de salidas mediante metodos (completar)
         totalTransporte = calcularTotalTransporte(); // aqui podrian faltar parametros
 	}
